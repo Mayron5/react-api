6692ef5236a93a438fea075dee7b6a03042e94eb
Mayron Alves <mayronalves@alu.ufc.br>
2022-07-26 19:13:06 -0300

    Método update do restaurante.

--- alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,16 +1,33 @@
 import { Button, TextField } from '@mui/material';
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import IRestaurante from '../../../interfaces/IRestaurante';
 
 const FormularioRestaurante = () => {
 
   const [nome, setNome] = useState('');
+  const parametros = useParams();
+
+  useEffect(() => {
+    if (parametros.id) {
+      axios.get<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`)
+        .then(response => {
+          setNome(response.data.nome);
+        });
+    }
+  }, [parametros]);
 
   const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
 
-    axios.post('http://localhost:8000/api/v2/restaurantes/', {nome: nome})
-      .then(() => console.log('cadastrado com sucesso!'));
+    if (parametros.id) {
+      axios.put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, { nome })
+        .then(() => console.log('atualizado com sucesso'));
+    } else {
+      axios.post('http://localhost:8000/api/v2/restaurantes/', { nome })
+        .then(() => console.log('cadastrado com sucesso!'));
+    }
 
   };
 
--- alfood-main/src/paginas/Administracao/Restaurantes/index.tsx
+++ alfood-main/src/paginas/Administracao/Restaurantes/index.tsx
@@ -1,6 +1,7 @@
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import IRestaurante from '../../../interfaces/IRestaurante';
 
 const AdiministracaoRestaurante = () => {
@@ -20,12 +21,16 @@ const AdiministracaoRestaurante = () => {
         <TableHead>
           <TableRow>
             <TableCell>Nome</TableCell>
+            <TableCell>Editar</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {restaudantes.map((restaurante => (
             <TableRow key={restaurante.id}>
               <TableCell>{restaurante.nome}</TableCell>
+              <TableCell>
+                <Link to={`/admin/restaurantes/${restaurante.id}`}>editar</Link>
+              </TableCell>
             </TableRow>
           )))}
         </TableBody>
